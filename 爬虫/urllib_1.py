9038080b352994093e2f61855ac953c510b69ca1
04141257@cumt.edu.cn <04141257@cumt.edu.cn>
2019-02-09 12:33:17 +0800

    commit

--- 爬虫/urllib_1.py
+++ 爬虫/urllib_1.py
@@ -1,4 +1,12 @@
 import urllib.request as ur
+import http.cookiejar
 
-response = ur.urlopen("http://www.baidu.com")
-print(response.read().decode('utf-8'))
+# response = ur.urlopen("http://www.baidu.com")
+# print(response.read().decode('utf-8'))
+
+cookie = http.cookiejar.CookieJar()
+handler = ur.HTTPCookieProcessor(cookie)
+opener = ur.build_opener(handler)
+response = opener.open("http://www.baidu.com")
+for item in cookie:
+    print(item.name + "=" + item.value)
